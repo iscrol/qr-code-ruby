bd3002f500a9016254a20e51a3da95c05418214c
iscrol <116039611+iscrol@users.noreply.github.com>
2025-01-23 21:19:33 +0000

    did qr demo

--- qr.rb
+++ qr.rb
@@ -1,1 +1,10 @@
-# Write your solution here!
+require "rqrcode"
+
+pp "What kind of url do you want a qr code for"
+user_input = gets.chomp
+
+qrcode = RQRCode::QRCode.new(user_input)
+
+png = qrcode.as_png({ :size => 500 })
+
+IO.binwrite("sometext.png", png.to_s)
